4a2c45b9ea4f8a67e91e023138480db208f40424
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-01-29 17:06:06 +0900

    Adding Local State to a Class.

--- StateAndLifecycle.js
+++ StateAndLifecycle.js
@@ -1,20 +1,20 @@
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() }
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello, React!!!</h1>
-                <h2>It is {this.props.date.toLocaleTimeString()}!!</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}!!</h2>
             </div>
         );
     }
 }
 
-function tick() {
-    const element = <Clock date={new Date()} />;
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
