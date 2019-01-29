485bc9c536e05363c6c0c227e68c5a02df7c251d
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-01-29 11:27:31 +0900

    calling a JavaScript function.

--- MainConcepts.js
+++ MainConcepts.js
@@ -1,5 +1,9 @@
-const name = 'Mark Kim';
-const element = <h1>hello, {name}</h1>
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = { firstName: 'Mark', lastName: 'Kim' };
+const element = <h1>hello, {formatName(user)}</h1>;
 
 ReactDOM.render(
     element,
