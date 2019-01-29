87d84ee7e326a512108a0d4ccfa3c5ebeb4945d8
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-01-29 15:54:23 +0900

    Extracted component AdditoinalInfo.

--- ComponentsAndProps.js
+++ ComponentsAndProps.js
@@ -2,6 +2,19 @@ function formatDate(date) {
     return date.toLocaleDateString();
 }
 
+function AdditoinalInfo(props) {
+    return (
+        <div>
+            <div>
+                {props.additoinalInfo.text}
+            </div>
+            <div>
+                {formatDate(props.additoinalInfo.date)}
+            </div>
+        </div>
+    );
+}
+
 function UserInfo(props) {
     return (
         <div>
@@ -19,23 +32,22 @@ function Avatar(props) {
     );
 }
 
+
+
 function Comment(props) {
     return (
         <div>
             <UserInfo user={props.user} />
-            <div>
-                {props.text}
-            </div>
-            <div>
-                {props.date}
-            </div>
+            <AdditoinalInfo additoinalInfo={props.additoinalInfo} />
         </div>
     );
 }
 
 const comment = {
-    date: new Date(),
-    text: 'Mark is studying React.',
+    additoinalInfo: {
+        date: new Date(),
+        text: 'Mark is studying React.',
+    },
     author: {
         name: 'Hello Kitty',
         url: 'https://placekitten.com/g/600/603',
@@ -45,8 +57,7 @@ const comment = {
 ReactDOM.render(
     <Comment
         user={comment.author}
-        text={comment.text}
-        date={formatDate(comment.date)}
+        additoinalInfo={comment.additoinalInfo}
     />,
     document.getElementById('root')
 );
