ec15561e2e392e2653f39b3fa9a594dcd950b67a
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-01-29 15:34:59 +0900

    Extracting Components.

--- ComponentsAndProps.js
+++ ComponentsAndProps.js
@@ -1,18 +1,40 @@
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+    return date.toLocaleDateString();
 }
 
-function App() {
+function Comment(props) {
     return (
         <div>
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
+            <div>
+                <img src={props.user.url} alt={props.user.name} />
+                <div>
+                    {props.user.name}
+                </div>
+            </div>
+            <div>
+                {props.text}
+            </div>
+            <div>
+                {props.date}
+            </div>
         </div>
     );
 }
 
+const comment = {
+    date: new Date(),
+    text: 'Mark is studying React.',
+    author: {
+        name: 'Hello Kitty',
+        url: 'https://placekitten.com/g/600/603',
+    },
+};
+
 ReactDOM.render(
-    <App />,
+    <Comment
+        user={comment.author}
+        text={comment.text}
+        date={formatDate(comment.date)}
+    />,
     document.getElementById('root')
 );
